38c2ce897cc144600e589ce26670e115362e0a8a
x23193689 <x23193689@student.ncirl.ie>
2024-04-21 20:13:00 +0100

    added many files

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -129,12 +129,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 
+# settings.py
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-
-
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'app/static'),
+    # Add other directories here
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
