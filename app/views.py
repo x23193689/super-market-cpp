c590b1fd47c441ec327cc35903e221a0b180f7fd
x23193689 <x23193689@student.ncirl.ie>
2024-04-22 11:55:08 +0100

    views chagnes for buynow

--- app/views.py
+++ app/views.py
@@ -6,6 +6,7 @@ from django.contrib import messages
 from django.db.models import Q
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 
 
